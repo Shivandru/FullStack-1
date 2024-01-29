957e537ef8d97a9e8ff2b4bfa676b61429f56fed
Shivandru <shivandru.singh045@gmail.com>
2024-01-29 23:20:08 +0530

    again

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://fullstack-three-chi.vercel.app"],
+    origin: ["https://fullstack-three-chi.vercel.app/"],
     credentials: true,
     httpOnly: true,
   })
